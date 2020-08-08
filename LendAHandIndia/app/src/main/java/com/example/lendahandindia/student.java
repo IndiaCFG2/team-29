1e7479b393b69cb61324c50b6111af10be78bd19
divyasimhadri <divyasimhadri7@gmail.com>
2020-08-08 21:43:37 +0530

    displaying lessons from firestore and profile edit

--- LendAHandIndia/app/src/main/java/com/example/lendahandindia/student.java
+++ LendAHandIndia/app/src/main/java/com/example/lendahandindia/student.java
@@ -14,13 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-<<<<<<< HEAD
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-=======
-import android.widget.Button;
->>>>>>> 43df8545a42ba8ed3d541149d929ee9951082b56
 import android.widget.TextView;
 
 import com.example.lendahandindia.Modal.lesson;
@@ -36,14 +30,11 @@ public class student extends AppCompatActivity {
 
     public static final String MY_PREFERENCE="com.example.lahi.user";
     FirebaseAuth mAuth;
-<<<<<<< HEAD
+
     RecyclerView mLessonlist;
     FirebaseFirestore db;
     FirestoreRecyclerAdapter<lesson,LessonViewHolder> adapter;
-=======
 
-    private Button profile;
->>>>>>> 43df8545a42ba8ed3d541149d929ee9951082b56
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +44,7 @@ public class student extends AppCompatActivity {
 
         setContentView(R.layout.activity_student);
 
-<<<<<<< HEAD
+
         mLessonlist=findViewById(R.id.lesson_list);
         FirebaseApp.initializeApp(getApplicationContext());
         LoadData();
@@ -135,15 +126,7 @@ public class student extends AppCompatActivity {
             resteacher.setText(chapter);
         }
 
-=======
-        profile=findViewById(R.id.profile);
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(student.this,ProfileActivity.class));
-            }
-        });
->>>>>>> 43df8545a42ba8ed3d541149d929ee9951082b56
+
 
     }
 
@@ -162,6 +145,10 @@ public class student extends AppCompatActivity {
             case R.id.queries:
                 Intent intent = new Intent(getApplicationContext(), DoubtsFourmActivity.class);
                 startActivity(intent);
+                return  true;
+            case R.id.pro:
+                Intent intent9 = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent9);
                 return  true;
             case R.id.logout:
                 mAuth.signOut();
