4bc13e856fc7496a06176d86e8d07a914b3af83b
divyasimhadri <divyasimhadri7@gmail.com>
2020-08-08 13:33:12 +0530

    main screen+ register

--- LendAHandIndia/app/src/main/java/com/example/lendahandindia/MainActivity.java
+++ LendAHandIndia/app/src/main/java/com/example/lendahandindia/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         registerbtn =(Button) findViewById(R.id.registerbtn);
     }
 
-    public void register()
+    public void register(View view)
     {
         Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
         startActivity(intent);
