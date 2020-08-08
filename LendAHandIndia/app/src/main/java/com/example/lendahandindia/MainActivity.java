0ade7a9740a477a74475faef0416e32fa498f7ac
divyasimhadri <divyasimhadri7@gmail.com>
2020-08-08 13:29:49 +0530

    basic+login

--- LendAHandIndia/app/src/main/java/com/example/lendahandindia/MainActivity.java
+++ LendAHandIndia/app/src/main/java/com/example/lendahandindia/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.lendahandindia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button registerbtn,signinbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerbtn =(Button) findViewById(R.id.registerbtn);
+    }
+
+    public void register()
+    {
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
     }
 }
